803c164fe084fbaae87679371ef89a68d45c76d5
Галеев Ильдар <ildar-galeev92@mail.ru>
2021-10-25 00:11:31 +0700

    Refactoring Program.cs

--- Program.cs
+++ Program.cs
@@ -3,11 +3,17 @@ using FactoryPattern.DrawPrimitives;
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Linq;
 
 namespace FactoryPattern
 {
     class Program
     {
+
+        static int windowWidth=150;
+        static int windowHeight=32;
+
+
         public static void Main(string[] args)
         {
             List<FireworkCreator> fireworkGuns = new List<FireworkCreator>();
@@ -15,14 +21,87 @@ namespace FactoryPattern
             List<IFirework> fireworks = new List<IFirework>();
             List<IFirework> deadFireworks = new List<IFirework>();
 
-            PixelList pixels = new PixelList();
-            Console.CursorVisible = false;
+            List<int> gunId = new List<int>(fireworkGuns.Count);
 
+            PixelList pixels = new PixelList();
+            
             bool run = true;
 
-            Random random = new Random(34356);
+            Random random = new Random(34356);    
 
-                for (int i = 0; i < 9; i++)
+            Console.CursorVisible = false;
+            Console.SetWindowSize(windowWidth, windowHeight);    
+
+            InitFireGuns(fireworkGuns);                
+            
+            Console.Clear();
+            
+            while (run)
+            {
+                if (gunId.Count==0)
+                {
+                    for (int i = 0; i < fireworkGuns.Count; i++)
+                    {
+                        gunId.Add(i);
+                    }
+                }
+
+                RedrawConsole(pixels, true);
+                
+                pixels.Clear();
+
+                foreach (var item in fireworkGuns)
+                {
+                    pixels.Add(item.Draw());
+                }
+
+                foreach (var fire in fireworks)
+                {
+                    var nextFrame = fire.NextFrame();
+                    if (nextFrame.Count == 0)
+                    {
+                        deadFireworks.Add(fire);
+                    }
+                    else
+                    {
+                        foreach (var item in nextFrame)
+                        {
+                            pixels.Add(item);
+                        }
+                    }
+                }
+
+
+                fireworks=fireworks.Except(deadFireworks).ToList();
+                deadFireworks.Clear();
+
+                RedrawConsole(pixels);
+                
+
+                Thread.Sleep(50);
+
+                if (Console.KeyAvailable)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.Escape: run = false; break;
+                        case ConsoleKey.F:
+                        int r = random.Next(0, gunId.Count - 1);
+                            
+                        fireworks.Add(fireworkGuns[gunId[r]].Fire());
+                        gunId.RemoveAt(r);
+                        break;
+                        default: break;
+                    }
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Firework show complete");
+        }
+
+        private static void InitFireGuns(List<FireworkCreator> fireworkGuns)
+        {
+            for (int i = 0; i < 9; i++)
                 {
                     if (i % 2 == 0)
                     {
@@ -33,82 +112,13 @@ namespace FactoryPattern
                         fireworkGuns.Add(new ConfettiGun((i + 1) * 15, 1, ConsoleColor.Green, new char[] { ':', 'x' }));
                     }
                 }
-
-                List<int> gunId = new List<int>(fireworkGuns.Count);
-            Console.SetWindowSize(150, 32);
-            Console.Clear();
-            
-                while (run)
-                {
-                if (gunId.Count==0)
-                {
-                    for (int i = 0; i < fireworkGuns.Count; i++)
-                    {
-                        gunId.Add(i);
-                    }
-                }
-
-                    RedrawConsole(pixels, true);
-                    pixels.Clear();
-
-                    foreach (var item in fireworkGuns)
-                    {
-                        pixels.Add(item.Draw());
-                    }
-
-                    foreach (var fire in fireworks)
-                    {
-                        var nextFrame = fire.NextFrame();
-                        if (nextFrame.Count == 0)
-                        {
-                            deadFireworks.Add(fire);
-                        }
-                        else
-                        {
-                            foreach (var item in nextFrame)
-                            {
-                                pixels.Add(item);
-                            }
-                        }
-                    }
-
-
-                    foreach (var item in deadFireworks)
-                    {
-                        fireworks.Remove(item);
-                    }
-                    deadFireworks.Clear();
-
-                    RedrawConsole(pixels);
-                   
-
-                    Thread.Sleep(50);
-
-                    if (Console.KeyAvailable)
-                    {
-                        switch (Console.ReadKey(true).Key)
-                        {
-                            case ConsoleKey.Escape: run = false; break;
-                            case ConsoleKey.F:
-                            int r = random.Next(0, gunId.Count - 1);
-                                
-                            fireworks.Add(fireworkGuns[gunId[r]].Fire());
-                            gunId.RemoveAt(r);
-                            break;
-                            default: break;
-                        }
-                    }
-            }
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("Firework show complete");
         }
 
         private static void RedrawConsole(PixelList pixels,bool clear=false)
-        {          
-                        
+        { 
             foreach (var item in pixels)
             {
-                if (item.Key.X >= 0&& item.Key.X<Console.BufferWidth && item.Key.Y >= 0 && item.Key.Y<Console.BufferHeight)
+                if (item.Key.X >= 0&& item.Key.X<windowWidth && item.Key.Y >= 0 && item.Key.Y<windowHeight)
                 {
                     Console.SetCursorPosition(item.Key.X, 30 - item.Key.Y);
                     if (clear)
@@ -119,8 +129,7 @@ namespace FactoryPattern
                     {
                         Console.ForegroundColor = item.Value.Color;
                         Console.Write(item.Value.Char);
-                    }
-                    
+                    }                    
                 }
             }
             Console.ResetColor();
